1ea826c88fa6fa1cd5b000999b553909124ef01d
raphael <raphael.silva31@fatec.sp.gov.br>
2025-07-18 21:27:47 -0300

    commit das alterações

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddDbContext<OrganizadorContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoPadrao")));
+Console.WriteLine(builder.Configuration.GetConnectionString("ConexaoPadrao"));
 
 builder.Services.AddControllers().AddJsonOptions(options =>
     options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
